260a5853ff621871b4ae8c3786ad562803662340
ChriswFoster <chriswfoster@gmail.com>
2018-05-08 15:10:27 -0500

    Starting to build my class out

---
+++ src/day1/examples/MyFirstClass.java
@@ -0,0 +1,19 @@
+package day1.examples;
+
+
+/*
+ * This is a 
+ * multiline comment :D 
+*/
+
+//Java classes will start with capital letters.
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System keyword starts with capital S
+		//Below is how you print something out to the console.
+		System.out.println("Oh billy.");
+	}
+
+}
