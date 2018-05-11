41000522b6191385775f335a17a56778ec483cfb
ChriswFoster <chriswfoster@gmail.com>
2018-05-11 12:34:42 -0500

    Exercise B complete

---
+++ src/day1/exercises/ExerciseC.java
@@ -0,0 +1,12 @@
+package day1.exercises;
+
+public class ExerciseC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Hello World";
+		System.out.println(a.substring(6, a.length()).toLowerCase());
+		
+	}
+
+}
