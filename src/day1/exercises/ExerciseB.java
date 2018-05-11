84a9c3a148810d4f01c4539e13066bf37588cbde
ChriswFoster <chriswfoster@gmail.com>
2018-05-11 11:58:45 -0500

    Exercise B complete

--- src/day1/exercises/ExerciseB.java
+++ src/day1/exercises/ExerciseB.java
@@ -4,7 +4,12 @@ public class ExerciseB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] nums = {"10", "20", "30", "40", "50"};
+		int sum = 0;
+		for(String number : nums ) {
+			sum += Integer.parseInt(number);
+		}
+		System.out.println(sum);
 	}
 
 }
